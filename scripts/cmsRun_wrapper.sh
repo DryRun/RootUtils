a898eb652d7d1969b4a62d7cf65b3bd74cfdd12d
DryRun <david.renhwa.yu@gmail.com>
2016-05-03 01:40:59 +0200

    Add scripts for making and executing cmsRun in condor

---
+++ scripts/cmsRun_wrapper.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# First argument ($1) = CMSSW release
+# Remaining arguments (${@:2}) are passed to cmsRun
+
+# send all output to stderr
+exec 1>&2
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export PYTHONHOME=/cvmfs/cms.cern.ch/slc6_amd64_gcc472/external/python/2.6.4/
+cmsrel "$1"
+tar -xzf src.tar.gz -C "$1/src"
+cd "$1/src"
+scram b
+cd -
+
+set -x
+
+echo "Starting at `date`"
+start=`date "+%s"`
+
+cmsRun "${@:2}"
+
+cmsRun_rc=$?
+
+echo "cmsRun exited with status $cmsRun_rc"
+
+ls -ltr
+
+# discard all output
+rm -f *.root
+
+echo "Finished at `date`"
+finish=`date "+%s"`
+echo Total runtime: $(($finish - $start)) seconds
+
+if [ "$cmsRun_rc" != "0" ]; then
+  echo "FAILURE"
+  exit 1
+fi
+
+echo "SUCCESS"
